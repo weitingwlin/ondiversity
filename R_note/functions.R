5ee9a1eb8f568ae5286996beac30330c9c69b6f0
weitingwlin <weiting.lin.lin@gmail.com>
2017-05-15 16:24:00 -0700

    test pull

---
+++ R_note/functions.R
@@ -0,0 +1,10 @@
+richness <- function(A){
+    return(length(unique(A[!is.na(A)])))
+}
+
+simpsonIER <- function(A){
+    TA <- table(A[!is.na(A)])
+    N <- sum(TA)          
+    IER <-(1 - sum((TA / N) ^ 2)) * N / (N - 1)
+    return(IER)
+}  
