70a1708c1e5428139e1c1f7e6a7ada28a904f651
Robin Wei <wei.robin@gmail.com>
2017-05-16 13:37:05 -0700

    Changed Clean Data and Added Sort Data

---
+++ python_note/cleanData.py
@@ -0,0 +1,20 @@
+'''
+ This file will import and clenup the data
+'''
+
+import numpy as np
+import pandas as pd
+
+def dataCleanUp(df = None):
+    
+    # Import data
+    if df is None:
+        file_name = "/Users/Robin/Documents/GitHub/ondiversity/Data/data_public1.csv"
+        df = pd.read_csv(file_name)
+    
+    # Delete column contains only "NaN"
+    cleaned_data = df.dropna(axis = 1, how = "all")
+    
+    # Delete row when EmplID = NaN
+    cleaned_data = cleaned_data[cleaned_data["EmplID"].notnull()]
+    return df;
